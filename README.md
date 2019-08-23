81a2dda4004d7bfc508c8b0f6000250f13c0fa59
Dmytro Kyrychuk <dmytro@kyrych.uk>
2019-08-23 17:05:01 +0300

    Add CrowdIn import script

--- README.md
+++ README.md
@@ -2,9 +2,34 @@
 Repository of Ukrainian localization for RimWorld.
 
 ## Як долучитися?
-У вас є два варіанти:
- - Долучитися до [проекту перекладу](https://crowdin.com/project/rimworld-ukr) на платформі CrowdIn. Варто зазначити, що туди перенесено ще не весь зроблений переклад, але, в цілому, він (тобто проект) відображає (теоретично) актуальний стан ресурсів гри.
- - Перекладати напряму файли на гітхабі. Як? Читайте на вікі. Одразу хочу попередити, що цей шлях складніший за попередній.
+
+1. Основним джерелом перекладів є
+  [проект](https://crowdin.com/project/rimworld-ukr) на платформі CrowdIn.
+  Усі нові переклади варто робити спершу там, задля уникнення розбіжностей
+  між перекладами у цьому репозиторі та на CrowdIn. Завантажити переклади з
+  CrowdIn у цей репозиторій можна за допомогою скрипта:
+  `./scripts/import-from-crowdin.py -p rimworld-urk -k abcde12345`. Ключ для
+  скрипта можна знайти у вкладці API налаштувань проекту на CrowdIn (доступна
+  для менеджерів проекту). Детальніше про параметри скрипта:
+
+  ```console
+  $ ./scripts/import-from-crowdin.py -h
+  usage: import-from-crowdin.py [-h] [-p PROJECT_IDENTIFIER] [-k PROJECT_KEY]
+
+  Replace local translations with latest version from CrowdIn.
+
+  optional arguments:
+    -h, --help            show this help message and exit
+    -p PROJECT_IDENTIFIER, --project-identifier PROJECT_IDENTIFIER
+                          Crowdin project identifier
+    -k PROJECT_KEY, --project-key PROJECT_KEY
+                          Crowdin API project key
+  ```
+
+  Для запуску скрипта потрібен Python 3.6+.
+
+2. Переклади також можна додавати напряму сюди. Це робити не бажано, бо такі
+  переклади може бути втрачено під час синхронізації перекладу з CrowdIn.
  
 В будь-якому випадку, насамперед вам варто ознайомитися із [вікі](https://github.com/Ludeon/RimWorld-Ukrainian/wiki).
 Також у нагоді стане посилання, розміщене нижче.
---
+++ scripts/import-from-crowdin.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import os.path
+import tempfile
+import urllib.request
+import zipfile
+
+
+def main(args):
+    parser = argparse.ArgumentParser(
+        description="Replace local files with latest version from CrowdIn."
+    )
+    parser.add_argument(
+        "-p",
+        "--project-identifier",
+        help="Crowdin project identifier",
+        default=os.environ.get("PROJECT_IDENTIFIER"),
+    )
+    parser.add_argument(
+        "-k",
+        "--project-key",
+        help="Crowdin API project key",
+        default=os.environ.get("PROJECT_KEY"),
+    )
+    parsed_args = parser.parse_args(args)
+    if not parsed_args.project_identifier:
+        raise
+    if not parsed_args.project_key:
+        raise
+
+    project = CrowdinProject(parsed_args.project_identifier, parsed_args.project_key)
+    archive = project.download_archive()
+    project.extract_archive(archive, os.getcwd())
+
+
+class CrowdinProject:
+    BASE_URL = "https://api.crowdin.com/api/project"
+
+    def __init__(self, project_identifier, project_key):
+        self.project_identifier = project_identifier
+        self.project_key = project_key
+
+    def download_archive(self):
+        url = f"{self.BASE_URL}/{self.project_identifier}/download/uk.zip?key={self.project_key}"
+        print(url)
+
+        (archive_path, _) = urllib.request.urlretrieve(url)
+        archive = zipfile.ZipFile(archive_path)
+        return archive
+
+    def extract_archive(self, archive: zipfile.ZipFile, target_dir):
+        prefix = "master/"
+        for info in archive.infolist():
+            print(info)
+            assert info.filename.startswith(prefix)
+            path = os.path.join(target_dir, info.filename[len(prefix) :])
+            if _is_dir(info):
+                os.makedirs(path, exist_ok=True)
+            else:
+                content = archive.read(info)
+                with open(path, "wb") as f:
+                    f.write(content)
+
+
+def _is_dir(zipinfo):
+    """Determine if zipinfo object corresponds to a directory.
+
+    It was observed that directories paths have a trailing slash.
+    """
+    return zipinfo.filename.endswith("/")
+
+
+if __name__ == "__main__":
+    import sys
+
+    main(sys.argv[1:])
